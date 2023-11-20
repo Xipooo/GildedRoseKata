8e2862ddbc8306f4a41f39c05d808488fefd98c5
Xipooo <xipooo@gmail.com>
2023-11-19 21:05:40 -0800

    Remove unnecessary curly braces and spaces

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -17,10 +17,7 @@ public class GildedRose
         {
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.Quality = item.Quality - 1;
-                }
+                if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros") item.Quality = item.Quality - 1;
             }
             else
             {
@@ -30,23 +27,13 @@ public class GildedRose
 
                     if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.SellIn < 11)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
+                        if (item.SellIn < 11) item.Quality = item.Quality + 1;
+                        if (item.SellIn < 6) item.Quality = item.Quality + 1;
                     }
                 }
             }
 
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.SellIn = item.SellIn - 1;
-            }
+            if (item.Name != "Sulfuras, Hand of Ragnaros") item.SellIn = item.SellIn - 1;
 
             if (item.SellIn < 0)
             {
@@ -54,22 +41,13 @@ public class GildedRose
                 {
                     if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
+                        if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros") item.Quality = item.Quality - 1;
                     }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
+                    else item.Quality = item.Quality - item.Quality;
                 }
                 else
                 {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
+                    if (item.Quality < 50) item.Quality = item.Quality + 1;
                 }
             }
         }
