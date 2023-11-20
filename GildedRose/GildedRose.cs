522727c3d972e5b6673a3ed444b1cdc1c8fef072
Xipooo <xipooo@gmail.com>
2023-11-20 15:05:47 -0800

    Move instantiation information to Dictionary

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GildedRoseKata;
 
@@ -6,10 +8,14 @@ public partial class GildedRose
 {
     private readonly IList<Item> _items;
 
-    public GildedRose(IList<Item> items)
-    {
-        _items = items;
-    }
+    public GildedRose(IList<Item> items) => _items = items;
+
+    public Dictionary<string, Func<Item, UpdatableItem>> UPDATABLEITEMS_TABLE = new() {
+            { "Aged Brie", (item) => new AgedBrie(item) },
+            { "Backstage passes to a TAFKAL80ETC concert", (item) => new BackstagePass(item) },
+            { "Sulfuras, Hand of Ragnaros", (item) => new UpdatableItem(item) },
+            { "Default", (item) => new NormalItem(item) }
+    };
 
     public void UpdateQuality()
     {
@@ -20,14 +26,6 @@ public partial class GildedRose
         }
     }
 
-    public UpdatableItem UpdateableItemFactory(Item item)
-    {
-        return item.Name switch
-        {
-            "Aged Brie" => new AgedBrie(item),
-            "Backstage passes to a TAFKAL80ETC concert" => new BackstagePass(item),
-            "Sulfuras, Hand of Ragnaros" => new UpdatableItem(item),
-            _ => new NormalItem(item)
-        };
-    }
+    public UpdatableItem UpdateableItemFactory(Item item) =>
+        UPDATABLEITEMS_TABLE.FirstOrDefault(updatableItem => updatableItem.Key.Equals(item.Name) || updatableItem.Key.Equals("Default")).Value(item);
 }
