4acab76670c5d9be32777e0e5e63573af8fe1d17
Xipooo <xipooo@gmail.com>
2023-11-20 13:57:51 -0800

    Move functions to classes with common interface

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -17,30 +17,91 @@ public class GildedRose
         {
             if (item.Name == "Aged Brie") AgedBrieUpdate(item);
             else if (item.Name == "Backstage passes to a TAFKAL80ETC concert") BackstagePassesUpdate(item);
-            else if (item.Name == "Sulfuras, Hand of Ragnaros") SulfrasUpdate(item);
+            else if (item.Name == "Sulfuras, Hand of Ragnaros") SulfurasUpdate(item);
             else NormalUpdate(item);
         }
     }
 
-    public void AgedBrieUpdate(Item item) {
-        if (item.Quality < 50) item.Quality = item.Quality + 1;
-        if (item.Quality < 50 && item.SellIn < 0) item.Quality = item.Quality + 1;
+
+    public interface IItemUpdate {
+        void Update();
+    }
+    
+    public class AgedBrie : IItemUpdate
+    {
+        private Item item;
+
+        public AgedBrie(Item item)
+        {
+            this.item = item;
+        }
+
+        public void Update()
+        {
+            if (item.Quality < 50) item.Quality = item.Quality + 1;
+            if (item.Quality < 50 && item.SellIn < 0) item.Quality = item.Quality + 1;
+        }
     }
 
-    public void BackstagePassesUpdate(Item item) {
-        if (item.Quality < 50) item.Quality = item.Quality + 1;
-        if (item.Quality < 50 && item.SellIn < 11) item.Quality = item.Quality + 1;
-        if (item.Quality < 50 && item.SellIn < 6) item.Quality = item.Quality + 1;
-        if (item.SellIn < 1) item.Quality = item.Quality - item.Quality;
+    public void AgedBrieUpdate(Item item)
+    {
+        new AgedBrie(item).Update();
     }
 
-    public void SulfrasUpdate(Item item) {
-        return;
+
+    public class BackstagePass : IItemUpdate {
+        private Item item;
+
+
+        public BackstagePass(Item item)
+        {
+            this.item = item;
+        }
+        public void Update() {
+            if (item.Quality < 50) item.Quality = item.Quality + 1;
+            if (item.Quality < 50 && item.SellIn < 11) item.Quality = item.Quality + 1;
+            if (item.Quality < 50 && item.SellIn < 6) item.Quality = item.Quality + 1;
+            if (item.SellIn < 1) item.Quality = item.Quality - item.Quality;
+        }
+    }
+    public void BackstagePassesUpdate(Item item)
+    {
+        new BackstagePass(item).Update();
     }
 
-    public void NormalUpdate(Item item) {
-        if (item.SellIn < 0 && item.Quality > 0) item.Quality = item.Quality - 1;
-        if (item.Quality > 0) item.Quality = item.Quality - 1;
-        item.SellIn = item.SellIn - 1;
+    public class Sulfurace : IItemUpdate
+    {
+        private Item item;
+        public Sulfurace(Item item)
+        {
+            this.item = item;
+        }
+        public void Update() { return; }
+    }
+
+    public void SulfurasUpdate(Item item)
+    {
+        new Sulfurace(item).Update();
+    }
+
+    public class NormalItem : IItemUpdate {
+        private Item item;
+
+
+        public NormalItem(Item item)
+        {
+            this.item = item;
+        }
+
+        public void Update() {
+            if (item.SellIn < 0 && item.Quality > 0) item.Quality = item.Quality - 1;
+            if (item.Quality > 0) item.Quality = item.Quality - 1;
+            item.SellIn = item.SellIn - 1;
+        }
+    }
+
+    public void NormalUpdate(Item item)
+    {
+        new NormalItem(item).Update();
     }
 }
